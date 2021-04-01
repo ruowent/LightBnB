bb4d04c35f5797aa8b2ec2eedb463b2ae7d32958
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-31 22:18:20 -0400

    Queries added

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,10 @@
+SELECT reservations.*, properties.*, avg(rating) AS average_rating
+FROM reservations
+JOIN properties ON properties.id = property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
+
